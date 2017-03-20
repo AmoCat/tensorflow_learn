a43eca5e4dbe8e66e3b7ccf74db6cba8627b6b52
AmoCat <jqzhu@ir.hit.edu.cn>
2017-03-20 15:04:58 +0800

    record prediction result

--- lstm/lstm.py
+++ lstm/lstm.py
@@ -17,10 +17,15 @@ flags.DEFINE_integer('epoch_size',1000,'batchs of each epoch')
 flags.DEFINE_integer('n_classes',46,'nums of classes')
 flags.DEFINE_integer('emb_size',345823,'embedding size')
 flags.DEFINE_integer('word_dim',100,'word dim')
+flags.DEFINE_integer('PRF',0,'calculate PRF')
 flags.DEFINE_float('learning_rate',1e-3,'learning rate')
 flags.DEFINE_float('dropout',0.5,'dropout')
 flags.DEFINE_string('data_path',None,'data path')
 flags.DEFINE_string('embedding_path','./embeddings','embedding_path')
+flags.DEFINE_string('saver_path','./model_saver','saver_path')
+flags.DEFINE_string('output_path','./output.txt','prediction_output_path')
+flags.DEFINE_string('label_dict_path','./int_to_label_dict','int_to_label_dict_path')
+flags.DEFINE_string('word_dict_path','./int_to_word','int_to_word_dict_path')
 
 def dynamic_rnn():
 	train_data = Dataset(data_type = 'train')
@@ -46,31 +51,64 @@ def dynamic_rnn():
 
 		logits = tf.matmul(outputs, weights) + biases
 		cost = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(logits, y_))
-		optimizer = optimizer = tf.train.AdamOptimizer(learning_rate = FLAGS.learning_rate).minimize(cost)
+		optimizer = tf.train.AdamOptimizer(learning_rate = FLAGS.learning_rate).minimize(cost)
 
-	correct_prediction = tf.nn.in_top_k(logits,y_,1)
+	correct_prediction = tf.nn.in_top_k(logits, y_, 1)
+	values, indices = tf.nn.in_top_k(logits, 1)
 	accuracy = tf.reduce_mean(tf.cast(correct_prediction,tf.float32))
 	correct = tf.reduce_sum(tf.cast(correct_prediction,tf.float32))
 
 	config = tf.ConfigProto()
 	config.gpu_options.allow_growth = True
-	with tf.Session(config = config) as sess:
-		sess.run(tf.initialize_all_variables())
-		#sess.run(optimizer,feed_dict = {x_:np.array([3,3]).reshape([1,-1]),y_:np.array([1,1]).reshape([-1]),output_keep_prob:0.5})
-		for step in range(FLAGS.epoch_size * FLAGS.epoch_step):
-			batch_x,batch_y = train_data.next_batch()
-			sess.run(optimizer, feed_dict = {x_:batch_x,y_:batch_y,output_keep_prob:1-FLAGS.dropout})
-			#if step % 1000 == 0:
-			#	print cost.eval(feed_dict = {x_:batch_x,y_:batch_y,output_keep_prob:1})
-			if step % FLAGS.epoch_size == 0:
-				num = 0
-				cor_num = 0
-				for i in range(0,test_data.sentence_num-1):
-					test_x, test_y = test_data.next_batch()
-					num += len(test_y)
-					cor_num += sess.run(correct,feed_dict = \
-					{x_:test_x, y_:test_y, output_keep_prob:1-FLAGS.dropout})
-				print "step %d , test_accuracy: %g" % (step,cor_num/num)
+
+	if FLAGS.PRF == 0:
+		with tf.Session(config = config) as sess:
+			saver = tf.train.Saver()
+			best_acc = 0
+			sess.run(tf.initialize_all_variables())
+			for step in range(FLAGS.epoch_size * FLAGS.epoch_step):
+				batch_x,batch_y = train_data.next_batch()
+				sess.run(optimizer, feed_dict = {x_:batch_x,y_:batch_y,output_keep_prob:1-FLAGS.dropout})
+				#if step % 1000 == 0:
+				#	print cost.eval(feed_dict = {x_:batch_x,y_:batch_y,output_keep_prob:1})
+				if step % FLAGS.epoch_size == 0:
+					num = 0
+					cor_num = 0
+					for i in range(0, test_data.sentence_num-1):
+						test_x, test_y = test_data.next_batch()
+						num += len(test_y)
+						_, correct_num = sess.run([optimizer,correct],feed_dict = \
+						{x_:test_x, y_:test_y, output_keep_prob:1-FLAGS.dropout})
+						cor_num += correct_num
+					test_accuracy = cor_num/num
+					if test_accuracy >= best_acc:
+						saver.save(sess,FLAGS.saver_path)
+					print "step %d , test_accuracy: %g" % (step,test_accuracy)
+	else:
+		with tf.Session(config = config) as sess:
+			saver = tf.train.Saver()
+			saver.restore(sess, FLAGS.saver_path)
+			label_dict = pkl.load(open(FLAGS.label_dict_path,'r'))
+			word_dict = pkl.load(open(FLAGS.word_dict_path,'r'))
+			out = open(FLAGS.output_path,'word_dict_path')
+			num = 0
+			cor_num = 0
+			for i in range(0, test_data.sentence_num-1):
+				test_x, test_y = test_data.next_batch()
+				num += len(test_y)
+
+				_, correct_num = sess.run([optimizer,correct],feed_dict = \
+						{x_:test_x, y_:test_y, output_keep_prob:1-FLAGS.dropout})
+				cor_num += correct_num
+
+				ind = indices.eval(feed_dict = {x_:test_x, y_:test_y, output_keep_prob:1-FLAGS.dropout})
+				for j in range(0,len(test_y)-1):
+					out.write(word_dict[test_x[i]] + '\t' + label_dict[test_y[i]] + '\t' + label_dict[ind[i]] + '\n')
+				out.write('\n')
+				out.flush()
+			print "test_accuracy: %g" % (step,test_accuracy)
+			out.close()
+
 
 def main(_):
 	dynamic_rnn()
